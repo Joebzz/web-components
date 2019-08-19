4bba93889c51e287a4f4cf6c0777eb02aae725fa
Joe Dawson <jdawson@Marine.ie>
2019-08-19 13:29:43 +0100

    Reenabled OL map

--- src/components/maps/openlayers/ol-layer-wms/ol-layer-wms.tsx
+++ src/components/maps/openlayers/ol-layer-wms/ol-layer-wms.tsx
@@ -1,7 +1,7 @@
 import { Component, Method, Prop } from '@stencil/core';
 
-// import ImageLayer from 'ol/layer/Image';
-// import ImageWMS from 'ol/source/ImageWMS';
+import ImageLayer from 'ol/layer/Image';
+import ImageWMS from 'ol/source/ImageWMS';
 
 @Component({
     tag: 'ol-layer-wms',
@@ -13,17 +13,17 @@ export class OlLayerWms {
     @Prop() serverType: string = 'geoserver';
     private _layer: any;
 
-    // componentDidLoad() {
-    //     this._layer = new ImageLayer({
-    //         //extent: [-13884991, 2870341, -7455066, 6338219],
-    //         source: new ImageWMS({
-    //             url: this.url,
-    //             params: { 'LAYERS': this.layer },
-    //             ratio: 1,
-    //             serverType: this.serverType
-    //         })
-    //     });
-    // }
+    componentDidLoad() {
+        this._layer = new ImageLayer({
+            //extent: [-13884991, 2870341, -7455066, 6338219],
+            source: new ImageWMS({
+                url: this.url,
+                params: { 'LAYERS': this.layer },
+                ratio: 1,
+                serverType: this.serverType
+            })
+        });
+    }
     
     @Method()
     async getLayer() {
