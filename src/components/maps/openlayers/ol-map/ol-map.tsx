229b61efa43cd4709a2332f6cddc551605c63966
Joe Dawson <jdawson@Marine.ie>
2019-08-19 13:13:45 +0100

    Commented out OL Map.

--- src/components/maps/openlayers/ol-map/ol-map.tsx
+++ src/components/maps/openlayers/ol-map/ol-map.tsx
@@ -1,11 +1,11 @@
 import { h, Component, Element, Prop } from '@stencil/core';
 
-import Map from 'ol/map';
-import View from 'ol/view';
+// import Map from 'ol/map';
+// import View from 'ol/view';
 // import Zoom from 'ol/control/Zoom';
-import TileLayer from 'ol/layer/tile';
-import OSM from 'ol/source/OSM';
-import { fromLonLat } from 'ol/proj';
+// import TileLayer from 'ol/layer/tile';
+// import OSM from 'ol/source/OSM';
+// import { fromLonLat } from 'ol/proj';
 
 @Component({
     tag: 'ol-map',
@@ -16,44 +16,44 @@ export class OLMap {
     @Prop() latitude: number;
     @Prop() longitude: number;
     @Prop() zoom: number;
-    private _map: Map;
-    private mapElement: HTMLElement;
+    // private _map: Map;
+    // private mapElement: HTMLElement;
 
-    protected componentDidLoad() {
-        if (this._map) return;
-        this.mapElement = this.el.querySelector('#map');
-        this._map = new Map({
-            loadTilesWhileAnimating: true,
-            view: new View({
-                center: fromLonLat([this.longitude, this.latitude]),
-                zoom: this.zoom
-            }),
-            layers: this.getBaseLayers(),
-            target: this.mapElement
-        });
-        this.addChildLayers();
-    }
+    // protected componentDidLoad() {
+    //     if (this._map) return;
+    //     this.mapElement = this.el.querySelector('#map');
+    //     this._map = new Map({
+    //         loadTilesWhileAnimating: true,
+    //         view: new View({
+    //             center: fromLonLat([this.longitude, this.latitude]),
+    //             zoom: this.zoom
+    //         }),
+    //         layers: this.getBaseLayers(),
+    //         target: this.mapElement
+    //     });
+    //     this.addChildLayers();
+    // }
 
-    getBaseLayers() {
-        let layers = [];
-        // Add a base layer    
-        layers.push(new TileLayer({
-            source: new OSM()
-        }));
-        return layers;
-    }
+    // getBaseLayers() {
+    //     let layers = [];
+    //     // Add a base layer    
+    //     layers.push(new TileLayer({
+    //         source: new OSM()
+    //     }));
+    //     return layers;
+    // }
 
-    addChildLayers() {
-        let children = this.el.querySelector('#mapLayers').childNodes;
-        for (let i = 0; i < children.length; i++) {
-            let child = (children[i] as any);
-            if (child.getLayer) {
-                child.getLayer().then((res) => {
-                    this._map.addLayer(res);
-                });
-            }
-        }
-    }
+    // addChildLayers() {
+    //     let children = this.el.querySelector('#mapLayers').childNodes;
+    //     for (let i = 0; i < children.length; i++) {
+    //         let child = (children[i] as any);
+    //         if (child.getLayer) {
+    //             child.getLayer().then((res) => {
+    //                 this._map.addLayer(res);
+    //             });
+    //         }
+    //     }
+    // }
 
     render() {
         return (
