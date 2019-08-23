72bd5d58d84b6456593b40281c84b607e06aa7cf
Joe Dawson <jdawson@Marine.ie>
2019-08-23 12:08:35 +0100

    Chart charts to use chart.js.

--- src/components/charts/line-chart/line-chart.tsx
+++ src/components/charts/line-chart/line-chart.tsx
@@ -1,8 +1,6 @@
 import { Component, Element, h } from '@stencil/core';
 
-import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import Chart from 'chart.js';
 
 @Component({
     tag: 'line-chart',
@@ -10,58 +8,45 @@ import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 })
 export class LineChart {
     @Element() private _element: HTMLElement;
-    private _chartElement: HTMLElement;
-    private _chart: am4charts.XYChart;
-
-    private populateChartData() {
-        let data = [];
-        let value = 50;
-        for (let i = 0; i < 300; i++) {
-            let date = new Date();
-            date.setHours(0, 0, 0, 0);
-            date.setDate(i);
-            value -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-            data.push({ date: date, value: value });
-        }
-
-        this._chart.data = data;
-    }
+    private _chart: Chart;
+    private _data: any = [65, 59, 80, 81, 56, 55, 40];
+    private _data2: any = [40, 55, 56, 84, 80, 59, 65];
 
     protected componentDidLoad() {
-        am4core.useTheme(am4themes_animated);
-        this._chartElement = this._element.querySelector('#chart');
-        console.log(this._chartElement);
-        this._chart = am4core.create(this._chartElement, am4charts.XYChart);
-
-        // Populate some Data
-        this.populateChartData();
-
-        // Create axes
-        var dateAxis = this._chart.xAxes.push(new am4charts.DateAxis());
-        dateAxis.renderer.minGridDistance = 60;
-
-        this._chart.yAxes.push(new am4charts.ValueAxis());
-
-        // Create series
-        var series = this._chart.series.push(new am4charts.LineSeries());
-        series.dataFields.valueY = "value";
-        series.dataFields.dateX = "date";
-        series.tooltipText = "{value}"
-
-        series.tooltip.pointerOrientation = "vertical";
-
-        this._chart.cursor = new am4charts.XYCursor();
-        this._chart.cursor.snapToSeries = series;
-        this._chart.cursor.xAxis = dateAxis;
-
-        //chart.scrollbarY = new am4core.Scrollbar();
-        this._chart.scrollbarX = new am4core.Scrollbar();
+        const canvas: HTMLCanvasElement = this._element.querySelector('#chart')
+        const context: CanvasRenderingContext2D = canvas.getContext('2d');
+        const chartConfig: Chart.ChartConfiguration =
+        {
+            "type": "line",
+            "data": {
+                "labels": ["January", "February", "March", "April", "May", "June", "July"],
+                "datasets":
+                    [
+                        {
+                            "label": "My First Dataset",
+                            "data": this._data,
+                            "fill": false,
+                            "borderColor": "rgb(75, 192, 192)",
+                            "lineTension": 0
+                        },
+                        {
+                            "label": "My Second Dataset",
+                            "data": this._data2,
+                            "fill": false,
+                            "borderColor": "#4287f5",
+                            "lineTension": 0
+                        }
+                    ]
+            }
+        };
+        this._chart = new Chart(context, chartConfig);
+        console.log(this._chart);
     }
 
     render() {
         return (
             <div>
-                <div id="chart"></div>
+                <canvas id="chart"></canvas>
             </div>
         );
     }
